cbb3aace849dd1f7e11304e0b2e7f4dd6126d238
VasilVasilev93 <vasilvasilev093@gmail.com>
2014-11-27 14:41:03 +0200

    THIS SHIT WORKS FINALLY

--- crawler.py
+++ crawler.py
@@ -1,15 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
 from urllib.parse import urljoin
-from create_pages import Page
-from create_website import Website
-from sqlalchemy import update
 
 scanned_urls = []
-outer_links = []
 titles = []
-ID = []
-count = 0
+outer_links = []
 
 
 def is_outer_url(url, base_url):
@@ -17,21 +12,17 @@ def is_outer_url(url, base_url):
         return False
     return True
 
+
 def prepare_link(url, href):
     return urljoin(url, href)
 
 
-def get_domain(url):
-    url = url.strip("http://www.")
-    return url
+def scan_page(url, base_url):
+    if url in scanned_urls or url in outer_links:
+        return
 
-
-def scan_page(session, url, base_url, count):
-    count += 1
-    if url in scanned_urls or url in outer_links or '#' in url:
-        if "#" in url:
-            outer_links.append(url)
-
+    if "#" in url:
+        outer_links.append(url)
         return
 
     r = requests.get(url)
@@ -39,31 +30,17 @@ def scan_page(session, url, base_url, count):
     soup = BeautifulSoup(html)
 
     print (url)
-
-    if soup.title is not None:
+    if soup.title:
         if url not in scanned_urls:
             scanned_urls.append(url)
             titles.append(soup.title.string)
-            if count == 1:
-                domain = get_domain(url)
-                session.add(Website(title=soup.title.string, url=url, domain=domain))
-                ID1 = session.query(Website.id).filter(Website.title == soup.title.string).one()
-                ID1 = ID1[0]
-                ID.append(ID1)
-                session.commit()
-            elif count > 1:
-                session.add(Page(title=soup.title.string, url=url, website_id=ID[0]))
-                session.commit()
 
         for link in soup.find_all("a"):
             new_link = prepare_link(url, link.get("href"))
             if not is_outer_url(new_link, base_url):
-                scan_page(session, new_link, base_url, count)
+                scan_page(new_link, base_url)
             elif is_outer_url(new_link, base_url):
-                #print ('out')
                 outer_links.append(new_link)
-                scan_page(session, new_link, base_url, count)
+                scan_page(new_link, base_url)
 
-    session.execute(update(Website).where(Website.id == ID[0]).values(pages_count=len(soup)))
-    session.commit()
-    print(count)
+    return scanned_urls
--- search_engine.py
+++ search_engine.py
@@ -1,6 +1,8 @@
 import crawler
+from create_pages import Page
+from create_website import Website
 from create_base import Base
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, update
 from sqlalchemy.orm import Session
 
 
@@ -11,11 +13,32 @@ def from_file_to_list(file):
     return websites
 
 
-def crawl(session, websites):
-    count = 0
+def get_domain(url):
+    url = url.strip("https://www.")
+    return url
+
+
+def fill_database(session, titles, scanned_urls):
+    site_domain = get_domain(scanned_urls[0])
+    session.add(Website(title=titles[0], url=scanned_urls[0], domain=site_domain))
+
+    for count in range(1, len(titles)):
+        ID = session.query(Website.id).filter(Website.title == titles[0]).all()
+        ID = ID[0][0]
+        session.add(Page(title=titles[count], url=scanned_urls[count], website_id=ID))
+    p_count = session.query(Page).filter(Website.id == ID).count()
+    p_count += 1  # Main page including
+    session.execute(update(Website).where(Website.id == ID).values(pages_count=p_count))
+    session.commit()
+
+
+def crawl(session, websites, titles, urls):
     for item in websites:
         base_url = item
-        crawler.scan_page(session, item, base_url, count)
+        crawler.scan_page(item, base_url)
+        fill_database(session, titles, urls)
+        del titles[:]
+        del urls[:]
 
 
 def main():
@@ -24,7 +47,7 @@ def main():
     session = Session(bind=engine)
 
     websites = from_file_to_list("the_internet.txt")
-    crawl(session, websites)
+    crawl(session, websites, crawler.titles, crawler.scanned_urls)
 
     session.commit()
 
